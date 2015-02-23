b3fdc4bfd29e724b9259b38002d1a9667db0670d
jlyneu <jly@ccs.neu.edu>
2015-02-23 02:19:03 -0500

    Adding necessary components for unit testing
    Moved ngDialog include script in index.html lower to fix small bug.
    Added test framework dependencies to package.json so others can npm install the dependencies.
    Added karma.conf.js file to configure Karma to run Jasmine unit tests.
    Added mainControllerSpec Jasmine file for unit testing the MainController.

---
+++ test/karma.conf.js
@@ -0,0 +1,38 @@
+module.exports = function(config){
+  config.set({
+
+    basePath : '../',
+
+    files : [
+      'https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js',
+      'http://maps.google.com/maps/api/js?sensor=false&libraries=places',
+      'plugins/masonry.pkgd.min.js',
+      'plugins/imagesloaded.pkgd.min.js',
+      'plugins/codebird.js',
+      'https://ajax.googleapis.com/ajax/libs/angularjs/1.3.11/angular.min.js',
+      'https://cdnjs.cloudflare.com/ajax/libs/angular.js/1.3.13/angular-mocks.js',
+      'plugins/ngDialog.min.js',
+      'plugins/angular-masonry-directive.js',
+      'app.js',
+      'test/unit/*.js'
+    ],
+
+    autoWatch : true,
+
+    frameworks: ['jasmine'],
+
+    browsers : ['Chrome'],
+
+    plugins : [
+            'karma-chrome-launcher',
+            'karma-firefox-launcher',
+            'karma-jasmine'
+            ],
+
+    junitReporter : {
+      outputFile: 'test_out/unit.xml',
+      suite: 'unit'
+    }
+
+  });
+};
